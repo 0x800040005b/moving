05109efbbf18ba8db3467588d1e92c7105698513
Vasya Klishch <klishchvas@gmail.com>
2022-01-04 20:11:08 +0200

    change project

---
+++ app/js/app.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let footerFormCustomCheckbox = document.querySelector('#footer-order__custom-checkbox'),
+        footerFormCheckbox = document.querySelector('#footer-order__checkbox-confidential');
+
+
+    //checkbox in footerFormOrder
+
+
+    if (footerFormCustomCheckbox) {
+        footerFormCustomCheckbox.addEventListener("click", function () {
+
+            checkCheckbox();
+
+        });
+    }
+
+    function checkCheckbox() {
+        if (footerFormCheckbox) {
+            if (footerFormCheckbox.checked) {
+                footerFormCheckbox.checked = false;
+            }
+            else{
+                footerFormCheckbox.checked = true;
+            }
+
+
+        }
+    }
+
+});
